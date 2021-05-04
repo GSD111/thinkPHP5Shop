9a564b284afeea42225dd6914868f396ccbd21ac
GSD <1399411420@qq.com>
2021-05-04 17:24:09 +0800

    '读取器封装'

--- application/api/model/Image.php
+++ application/api/model/Image.php
@@ -6,5 +6,16 @@ use think\Model;
 
 class Image extends Model
 {
-    protected $hidden=['id','from','delete_time','update_time'];
+    protected $hidden = ['id', 'from', 'delete_time', 'update_time'];
+
+    //url读取器
+    public function getUrlAttr($value, $data)
+    {
+        $finalUrl = $value;
+        if ($data['from'] == 1) {
+            $finalUrl = config('setting.img_prefix') . $value;
+        }
+
+        return $finalUrl;
+    }
 }
