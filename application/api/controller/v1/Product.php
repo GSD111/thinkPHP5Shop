aa7ba01a0fcfd564ab454c5e91ffbb15c62c12f6
GSD <1399411420@qq.com>
2021-05-05 22:48:20 +0800

    '分类商品接编写'

--- application/api/controller/v1/Product.php
+++ application/api/controller/v1/Product.php
@@ -6,11 +6,17 @@ namespace app\api\controller\v1;
 
 use app\api\model\Product as ProductModel;
 use app\api\validate\Count;
+use app\api\validate\IDMustBePositiveInt;
 use app\lib\exception\ProductException;
 
 class Product
 {
-    public function getRecent($count=15)
+
+    /*
+     * $count 获取的数量 默认获取15条
+     * 获取最新产品的信息
+     */
+    public function getRecent($count = 15)
     {
         (new Count())->goCheck();
         $products = ProductModel::getMostRecent($count);
@@ -19,6 +25,24 @@ class Product
         }
 //        $collection= collection($products);
         $products = $products->hidden(['summary']);
+        return $products;
+    }
+
+    /*
+     * @id 分类的ID
+     * 获取分类下面的商品信息
+     */
+    public function getAllInCategory($id)
+    {
+//        (new IDMustBePositiveInt())->goCheck();
+
+        $products = ProductModel::getProductsByCategoryID($id);
+        if ($products->isEmpty()) {
+            throw new ProductException();
+        }
+
+        $products = $products->hidden(['summary']);
+
         return $products;
     }
 }
--- application/api/model/Product.php
+++ application/api/model/Product.php
@@ -8,14 +8,36 @@ class Product extends BaseModel
 {
     protected $hidden = ['delete_time', 'category_id', 'from', 'update_time', 'create_time', 'pivot'];
 
+    /*
+     * $value 所要读取的图片值
+     * $data 数据集合
+     * 产品图片读取器
+     */
     public function getMainImgUrlAttr($value, $data)
     {
         return $this->prefixImgUrl($value, $data);
     }
 
-    public static function getMostRecent($count){
+    /*
+     * $count 产品的数量
+     * 获取最新产品的数量
+     */
+    public static function getMostRecent($count)
+    {
 
         $products = self::limit($count)->order('create_time desc')->select();
+
+        return $products;
+    }
+
+    /*
+     * $categoryId  分类的ID
+     * 根据分类ID获取对应分类下的商品信息
+     */
+    public static function getProductsByCategoryID($categoryId)
+    {
+
+        $products = self::where('category_id', '=', $categoryId)->select();
 
         return $products;
     }
